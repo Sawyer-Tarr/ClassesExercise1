608c8e2a6f2c126b6a12b746c074e8dd42f1959e
sawyer tarr <sawyertarr@outlook.com>
2023-02-14 09:02:33 -0600

    FirstCommit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassesExercise;
+
+Car myCar = new Car()
+{
+    Make = "Chevrolete",
+    Model = "Silverado",
+    Year = 2015
+};
+
+Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
